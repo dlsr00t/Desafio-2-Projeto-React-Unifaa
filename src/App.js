2eea197e84507fe93320cd7fe0ec1fa7f99e73ca
Foureyes(Douglas) <dlsroot12@gmail.com>
2024-03-26 22:20:49 -0300

    salvar token, obter token, salvar usuario, obter usuario

--- src/App.js
+++ src/App.js
@@ -9,7 +9,32 @@ function App() {
     const campoSenha = document.querySelector("#senha");
     const btnEntrar = document.getElementById("btn-entrar");
 
-
+    const salvarToken = (token) => {
+        // Aqui salvo o usuario na Store
+        localStorage.setItem('token', token)
+    }
+    
+    const obterToken = (token) => {
+        // Obtenho o token da store
+       return localStorage.getItem("token");
+    }
+    
+    const salvarUsuario = (usuario) => {
+        // Aqui pego o usuario0 em obj e converto ele para json e salvo n a store.
+       return localStorage.setItem('usuario', JSON.stringify(usuario));
+    }
+    
+    const obterUsuario = (usuario) => {
+        // pego o usuario como json na store e converto ele para objeto e devolvo no return.
+        let usuarioStore = localStorage.getItem("usuario");
+        return JSON.parse(usuarioStore);
+    }
+    
+    const sairDoSistema = () => {
+        localStorage.removeItem('token'); 
+        localStorage.removeItem('usuario');
+        window.open('login.html', '_self');
+    }
 
     console.log("S={Douglas}")
     
